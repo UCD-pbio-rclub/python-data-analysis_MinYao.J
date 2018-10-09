e8355dac73cfb3c594e6adc44ef379bf85763617
MinYaoJhu <minjhu@ucdavis.edu>
2018-10-09 15:13:51 -0700

    test

---
+++ chapter 1/hello_world.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Hello,world!')
+
+
+# In[2]:
+
+
+an_apple = 27
+
+
+# In[3]:
+
+
+an_example = 42
+
+
+# In[4]:
+
+
+an_apple
+
+
+# In[5]:
+
+
+b = [1, 2, 3]
+
+
+# In[6]:
+
+
+b.reverse
+
+
+# In[7]:
+
+
+import datetime
+
+
+# In[8]:
+
+
+datetime.datetime
+
+
+# In[9]:
+
+
+get_ipython().run_line_magic('pinfo', 'b')
+
+
+# In[10]:
+
+
+get_ipython().run_line_magic('pinfo', 'print')
+
+
+# In[1]:
+
+
+get_ipython().run_line_magic('magic', '')
+
+
+# In[2]:
+
+
+def add_numbers(a,b)
+return a + b
+
+
+# In[3]:
+
+
+def add_numbers(a, b):
+    """
+    Add two numbers together
+    Returns
+    -------
+    the_sum : type of arguments
+    """
+    return a + b
+
+
+# In[4]:
+
+
+add_numbers(3, 5)
+
+
+# ### new head
+# 
+# ```python
+# 
+# add_numbers(3, 5)
+# 
+# ```
+
+# In[5]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[6]:
+
+
+get_ipython().run_line_magic('paste', '')
+
+
+# In[7]:
+
+
+get_ipython().run_line_magic('cpaste', '')
+
+
+# In[ ]:
+
+
+
+
